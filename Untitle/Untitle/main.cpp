cddc6d428098f538a4572bc534a6ab616294837d
poonasq257 <poonasp257@naver.com>
2018-10-11 17:55:03 +0900

    광원, 원통 메쉬
    DirectX 9

--- Untitle/Untitle/main.cpp
+++ Untitle/Untitle/main.cpp
@@ -2,57 +2,55 @@
 #include <mmsystem.h>
 #include <d3dx9.h>
 
+LPDIRECT3D9	g_pD3D = NULL;
+LPDIRECT3DDEVICE9 g_pd3dDevice = NULL;
+LPDIRECT3DVERTEXBUFFER9 g_pVB = NULL;
+
 struct CUSTOMVERTEX
 {
-	FLOAT x, y, z, rhw;
-	DWORD color;
+	D3DXVECTOR3 position;
+	D3DXVECTOR3 normal;
 };
 
-#define D3DFVF_CUSTOMVERTEX (D3DFVF_XYZRHW | D3DFVF_DIFFUSE) 
-
-LPDIRECT3D9	g_pD3D = nullptr;
-LPDIRECT3DDEVICE9 g_pd3dDevice = nullptr;
-LPDIRECT3DVERTEXBUFFER9 g_pVB = nullptr;
+#define D3DFVF_CUSTOMVERTEX (D3DFVF_XYZ | D3DFVF_NORMAL) 
 
 HRESULT InitD3D(HWND hWnd)
 {
-	if (!(g_pD3D = Direct3DCreate9(D3D_SDK_VERSION))) { return E_FAIL; }
-
+	if ((g_pD3D = Direct3DCreate9(D3D_SDK_VERSION)) == NULL) return E_FAIL;
+	
 	D3DPRESENT_PARAMETERS d3dpp;
 	ZeroMemory(&d3dpp, sizeof(d3dpp));
 	d3dpp.Windowed = TRUE;
 	d3dpp.SwapEffect = D3DSWAPEFFECT_DISCARD;
 	d3dpp.BackBufferFormat = D3DFMT_UNKNOWN;
+	d3dpp.EnableAutoDepthStencil = TRUE;
+	d3dpp.AutoDepthStencilFormat = D3DFMT_D16;
 
 	if (FAILED(g_pD3D->CreateDevice(D3DADAPTER_DEFAULT, D3DDEVTYPE_HAL, hWnd,
-		D3DCREATE_SOFTWARE_VERTEXPROCESSING, &d3dpp, &g_pd3dDevice))) {
-		return E_FAIL;
-	}
+		D3DCREATE_SOFTWARE_VERTEXPROCESSING, &d3dpp, &g_pd3dDevice))) return E_FAIL;
 
 	g_pd3dDevice->SetRenderState(D3DRS_CULLMODE, D3DCULL_NONE);
-	g_pd3dDevice->SetRenderState(D3DRS_LIGHTING, FALSE);
+	g_pd3dDevice->SetRenderState(D3DRS_ZENABLE, TRUE);
 
 	return S_OK;
 }
 
-HRESULT InitVertex()
+HRESULT InitGeometry()
 {
-	CUSTOMVERTEX vertices[] =
+	if (FAILED(g_pd3dDevice->CreateVertexBuffer(50 * 2 * sizeof(CUSTOMVERTEX),
+		0, D3DFVF_CUSTOMVERTEX, D3DPOOL_DEFAULT, &g_pVB, NULL))) return E_FAIL;
+		
+	CUSTOMVERTEX* pVertices;
+	if (FAILED(g_pVB->Lock(0, 0, (void**)&pVertices, 0))) return E_FAIL;
+
+	for (DWORD i = 0; i < 50; i++)
 	{
-		{ 150.0f, 50.0f, 0.5f, 1.0f, 0xffff0000, },
-		{ 250.0f, 250.0f, 0.5f, 1.0f, 0xff00ff00, },
-		{ 50.0f, 250.0f, 0.5f, 1.0f, 0xff00ffff, },
-	};
-
-	if (FAILED(g_pd3dDevice->CreateVertexBuffer(3 * sizeof(CUSTOMVERTEX),
-		0, D3DFVF_CUSTOMVERTEX, D3DPOOL_DEFAULT, &g_pVB, NULL))) {
-		return E_FAIL;
+		FLOAT theta = (2 * D3DX_PI * i) / (50 - 1);
+		pVertices[2 * i + 0].position = D3DXVECTOR3(sinf(theta), -1.0f, cosf(theta));
+		pVertices[2 * i + 0].normal = D3DXVECTOR3(sinf(theta), 0.0f, cosf(theta));
+		pVertices[2 * i + 1].position = D3DXVECTOR3(sinf(theta), 1.0f, cosf(theta));
+		pVertices[2 * i + 1].normal = D3DXVECTOR3(sinf(theta), 0.0f, cosf(theta));
 	}
-
-	VOID* pVertices;
-	if (FAILED(g_pVB->Lock(0, sizeof(vertices), (void**)&pVertices, 0))) { return E_FAIL; }
-
-	memcpy(pVertices, vertices, sizeof(vertices));
 	g_pVB->Unlock();
 
 	return S_OK;
@@ -60,18 +58,16 @@ HRESULT InitVertex()
 
 VOID Cleanup()
 {
-	if (g_pVB) { g_pVB->Release(); }
-	if (!g_pd3dDevice) { g_pd3dDevice->Release(); }
-	if (!g_pD3D) { g_pD3D->Release(); }
+	if (g_pVB != NULL) g_pVB->Release();
+	if (g_pd3dDevice != NULL) g_pd3dDevice->Release();
+	if (g_pD3D != NULL) g_pD3D->Release();
 }
 
 VOID SetupMatrices()
 {
 	D3DXMATRIXA16 matWorld;
-
-	UINT iTime = timeGetTime() % 1000;
-	FLOAT fAngle = iTime * (2.0f * D3DX_PI) / 1000.0f;
-	D3DXMatrixRotationY(&matWorld, fAngle);
+	D3DXMatrixIdentity(&matWorld);
+	D3DXMatrixRotationX(&matWorld, timeGetTime() / 500.0f);
 	g_pd3dDevice->SetTransform(D3DTS_WORLD, &matWorld);
 
 	D3DXVECTOR3 vEyePt(0.0f, 3.0f, -5.0f);
@@ -86,20 +82,46 @@ VOID SetupMatrices()
 	g_pd3dDevice->SetTransform(D3DTS_PROJECTION, &matProj);
 }
 
+VOID SetupLights()
+{
+	D3DMATERIAL9 mtrl;
+	ZeroMemory(&mtrl, sizeof(D3DMATERIAL9));
+	mtrl.Diffuse.r = mtrl.Ambient.r = 1.0f;
+	mtrl.Diffuse.g = mtrl.Ambient.g = 1.0f;
+	mtrl.Diffuse.b = mtrl.Ambient.g = 0.0f;
+	mtrl.Diffuse.a = mtrl.Ambient.g = 1.0f;
+	g_pd3dDevice->SetMaterial(&mtrl);
+
+	D3DXVECTOR3 vecDir;
+	D3DLIGHT9 light;
+	ZeroMemory(&light, sizeof(D3DLIGHT9));
+	light.Type = D3DLIGHT_DIRECTIONAL;
+	light.Diffuse.r = 1.0f;
+	light.Diffuse.g = 1.0f;
+	light.Diffuse.b = 1.0f;
+	vecDir = D3DXVECTOR3( cosf(timeGetTime() / 350.0f), 1.0f, sinf(timeGetTime() / 350.0f) );
+	D3DXVec3Normalize((D3DXVECTOR3*)&light.Direction, &vecDir);
+	light.Range = 1000.0f;
+	g_pd3dDevice->SetLight(0, &light);
+	g_pd3dDevice->LightEnable(0, TRUE);
+	g_pd3dDevice->SetRenderState(D3DRS_LIGHTING, TRUE);
+	g_pd3dDevice->SetRenderState(D3DRS_AMBIENT, 0x00202020);
+}
+
 VOID Render()
 {
-	if (!g_pd3dDevice) { return; }
 
-	g_pd3dDevice->Clear(0, NULL, D3DCLEAR_TARGET,
+	g_pd3dDevice->Clear(0, NULL, D3DCLEAR_TARGET | D3DCLEAR_ZBUFFER,
 		D3DCOLOR_XRGB(0, 0, 255), 1.0f, 0);
 	
 	if (SUCCEEDED(g_pd3dDevice->BeginScene()))
 	{
+		SetupLights();
 		SetupMatrices();
 
 		g_pd3dDevice->SetStreamSource(0, g_pVB, 0, sizeof(CUSTOMVERTEX));
 		g_pd3dDevice->SetFVF(D3DFVF_CUSTOMVERTEX);
-		g_pd3dDevice->DrawPrimitive(D3DPT_TRIANGLESTRIP, 0, 1);
+		g_pd3dDevice->DrawPrimitive(D3DPT_TRIANGLESTRIP, 0, 2 * 50 - 2);
 
 		g_pd3dDevice->EndScene();
 	}
@@ -127,13 +149,13 @@ INT WINAPI WinMain(HINSTANCE hInst, HINSTANCE, LPSTR, INT)
 		L"D3D Tutorial", NULL };
 	RegisterClassEx(&wc);
 
-	HWND hWnd = CreateWindow(L"D3D Tutorial", L"D3D Tutorial 01: CreateDevice",
+	HWND hWnd = CreateWindow(L"D3D Tutorial", L"D3D Tutorial",
 		WS_OVERLAPPEDWINDOW, 100, 100, 300, 300,
 		GetDesktopWindow(), NULL, wc.hInstance, NULL);
 
 	if (SUCCEEDED(InitD3D(hWnd)))
 	{
-		if (SUCCEEDED(InitVertex()))
+		if (SUCCEEDED(InitGeometry()))
 		{
 			ShowWindow(hWnd, SW_SHOWDEFAULT);
 			UpdateWindow(hWnd);
